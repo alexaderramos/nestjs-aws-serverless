2b89aa51db40b83af490e565ca269ed6b1c8a11f
Cristian Ramos <tcristian_15.go@hotmail.com>
2024-04-17 14:44:10 -0500

    fix swagger serverless

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,16 +1,37 @@
-import serverlessExpress from '@codegenie/serverless-express';
 import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@codegenie/serverless-express';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Callback, Context, Handler } from 'aws-lambda';
 
-let cachedServer;
+let server: Handler;
 
-export const handler = async (event: any, context: any) => {
-  if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
-    await nestApp.init();
-    cachedServer = serverlessExpress({
-      app: nestApp.getHttpAdapter().getInstance(),
-    });
-  }
-  return cachedServer(event, context);
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { logger: ['error'] });
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Api test')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('api_')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  //end swagger
+
+  // await app.listen(3000);
+  app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
 };
