e4cea66a22c698d30c814a5345edb87a3064decf
Cristian Ramos <tcristian_15.go@hotmail.com>
2024-04-18 03:31:19 -0500

    add server to swagger

--- src/lambda.ts
+++ src/lambda.ts
@@ -18,6 +18,10 @@ async function bootstrap() {
     .setDescription('The API description')
     .setVersion('1.0')
     .addTag('api_')
+    .addServer(
+      'hhtps://a5sgrg6mii.execute-api.us-east-1.amazonaws.com/dev',
+      'AWS Lambda',
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
